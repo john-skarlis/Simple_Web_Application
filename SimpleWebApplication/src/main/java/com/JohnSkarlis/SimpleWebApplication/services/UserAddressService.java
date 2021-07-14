ef298d5f08398f4b60170e960179243ee9686153
john-skarlis <skarlisgannis@gmail.com>
2021-07-14 17:59:16 +0300

    cleanning

--- SimpleWebApplication/src/main/java/com/JohnSkarlis/SimpleWebApplication/services/UserAddressService.java
+++ SimpleWebApplication/src/main/java/com/JohnSkarlis/SimpleWebApplication/services/UserAddressService.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class UserAddressService {
 
