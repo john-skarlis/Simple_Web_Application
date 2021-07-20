d09c3128bed5d8dbd805439f2c47ee57a265cd26
john-skarlis <skarlisgannis@gmail.com>
2021-07-20 14:21:53 +0300

    frontEnd

--- FrontEnd/simple_web_app/src/components/layout/MainNavigation.js
+++
@@ -1,9 +0,0 @@
-function MainNavigation() {
-  return (
-    <header className="sm h-auto p-3 grid justify-items-center bg-black bg-opacity-50">
-      <div className="text-3xl font-bold text-white">Simple Web App</div>
-    </header>
-  );
-}
-
-export default MainNavigation;
--- FrontEnd/simple_web_app/src/pages/HomePage.js
+++
@@ -1,8 +0,0 @@
-import HomeContent from "../components/HomeContent.js";
-import MainBox from "../components/layout/MainBox.js";
-
-function HomePage() {
-  return <MainBox title="Home Page" content= {<HomeContent/>} />;
-}
-
-export default HomePage;
